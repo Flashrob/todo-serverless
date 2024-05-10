351198f94dda47fc470ce719706c4f24a2d51e08
Flashrob <kolsek.income@gmail.com>
2024-05-10 23:01:32 +0800

    Fix list key error

--- src/app/components/List.tsx
+++ src/app/components/List.tsx
@@ -1,5 +1,5 @@
 "use client"
 
 export const List = ({todos}: {todos: { title: string}[]}) => {
-  return <>{todos.map((todo) => <p>{todo.title}</p>)}</>
+  return <>{todos.map((todo) => <p key={todo.title}>{todo.title}</p>)}</>
 }
