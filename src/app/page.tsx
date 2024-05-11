04d1cf50287a6a3a1bf7577d1b7f45208a54667b
Flashrob <kolsek.income@gmail.com>
2024-05-11 11:13:09 +0800

    Add button to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import { AddButton } from "./components/AddButton";
 import { List } from "./components/List";
 
 export default function Home() {
-  const todos = [{ title: "laundry"}, { title: "dishes"}]
+  const todos = [{ title: "laundry" }, { title: "dishes" }];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
       <div className="z-10 w-full max-w-5xl font-mono text-sm">
         Serverless Todo App
-        <List todos={todos}/>
+        <List todos={todos} />
+      </div>
+      <div className="flex w-full justify-end ">
+        <AddButton />
       </div>
     </main>
   );
